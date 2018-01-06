15cbd9d62979716142e1113ead8086d1b79aa997
wangxiaoqing <xiaoqingwang@126.com>
2018-01-06 15:47:12 +0800

    api

--- deploy/srv/src/router/views.js
+++ deploy/srv/src/router/views.js
@@ -3,6 +3,10 @@ const startviews = (app)=>{
   app.get('/', (req, res)=> {
     	res.render('index', { title: 'Express' });
   });
+
+  app.get('/api', (req, res)=> {
+      res.render('index', { title: 'Express' });
+  });
 };
 
 module.exports= startviews;
