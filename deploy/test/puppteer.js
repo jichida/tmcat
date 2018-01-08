1a3add2cb4ac1b18fb80d3b726a2df70ef8a93e6
wangxiaoqing <xiaoqingwang@126.com>
2018-01-08 20:44:45 +0800

    ===

--- deploy/test/puppteer.js
+++ deploy/test/puppteer.js
@@ -33,7 +33,7 @@ console.log(`===>`);
     console.log('browser2==>');
     await page.goto('http://tmcat.czjcd.com/info/5a532bf5ca1c890001fb4d43', {waitUntil: 'networkidle2'});
     console.log('browser3==>');
-    await page.screenshot({path: '/root/tmcat/deploy/dist/uploader/hn.png'});
+    await page.screenshot({path: './hn.png'});
     console.log('browser4==>');
     await browser.close();
     console.log('browser5==>');
