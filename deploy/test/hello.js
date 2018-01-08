45f5ea5a3a04f75b633fe69006951aa25b01d9dc
wangxiaoqing <xiaoqingwang@126.com>
2018-01-08 22:12:17 +0800

    tomcat

--- deploy/test/hello.js
+++ deploy/test/hello.js
@@ -1,13 +1,15 @@
 var page = require('webpage').create();
 //viewportSize being the actual size of the headless browser
-page.viewportSize = { width: 1024, height: 768 };
+page.viewportSize = {    width: 640,
+    height: 1460 };
 //the clipRect is the portion of the page you are taking a screenshot of
-page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
+page.clipRect = { top: 0, left: 0,     width: 640,
+    height: 1460 };
 //the rest of the code is the same as the previous example
 console.log('hello');
 console.log('你好');
 
-page.open('http://www.baidu.com/', function() {
-  page.render('/root/tmcat/deploy/dist/uploader/baidu.png');
+page.open('http://tmcat.czjcd.com/infohidden/5a532bf5ca1c890001fb4d43', function() {
+  page.render('/root/tmcat/deploy/dist/uploader/tmcat.png');
   phantom.exit();
 });
