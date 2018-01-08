8e144823bf993b22fe3c78e6e936dd6948ae9fd0
wangxiaoqing <xiaoqingwang@126.com>
2018-01-08 23:28:01 +0800

    no message

--- deploy/test/phantom.js
+++ deploy/test/phantom.js
@@ -1,19 +1,28 @@
 
 const phantom = require('phantom');
 
-phantom.create().then(function(instance){
-  console.log('get instance!!');
-  instance.createPage().then(function(page){
-    console.log('get page!!');
-    page.open('http://tmcat.czjcd.com/infohidden/5a532bf5ca1c890001fb4d43', function(){
-      console.log('get open');
-      page.render('/root/tmcat/deploy/dist/uploader/tmcat2.jpg');
-      instance.exit();
-      console.log('====>finish!!');
-    });
-  })
-}).catch((e)=>{
-  console.log(e);
+const snapimage = (url,callbackfn)=>{
+  phantom.create().then(function(instance){
+    console.log('get instance!!');
+    instance.createPage().then(function(page){
+      console.log('get page!!');
+       page.property('viewportSize', { width: 640, height: 1460 }).then(function(){
+         page.open(url, function(status){
+           console.log('get open');
+           page.render('/root/tmcat/deploy/dist/uploader/tmcat2.jpg');
+           instance.exit();
+           console.log('====>finish!!');
+         });
+       });
+    })
+  }).catch((e)=>{
+    console.log(e);
+    callbackfn(e,null);
+  });
+};
+
+snapimage('http://tmcat.czjcd.com/infohidden/5a532bf5ca1c890001fb4d43',(err,result)=>{
+  console.log(result);
 });
 // (async function() {
 //   const instance = await phantom.create();
