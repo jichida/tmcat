5af63e5cab834408e037684e0741ee6d94e16049
wangxiaoqing <xiaoqingwang@126.com>
2018-01-09 00:27:56 +0800

    +webbot

--- deploy/srv/src/handler/snapimage.js
+++ deploy/srv/src/handler/snapimage.js
@@ -8,7 +8,7 @@ const snapimage = (url,callbackfn)=>{
   const filename = `result_${uuid.v4()}.jpg`;
   const filepath = `${uploadDir}/${filename}`;
   const returi = `${config.uploadurl}/${filename}`;
-  console.log(`打开${url}\n目标文件:${filepath}\n文件返回地址:${returi}`)
+  console.log(`phantom->打开${url}\n目标文件:${filepath}\n文件返回地址:${returi}`)
   phantom.create().then(function(instance){
     instance.createPage().then(function(page){
        page.property('viewportSize', { width: 640, height: 1460 }).then(function(){
---
+++ deploy/srv/src/handler/webshot.js
@@ -0,0 +1,27 @@
+const uuid = require('uuid');
+const path = require('path');
+const config = require('../config.js');
+const webshot = require('webshot');
+
+const snapimage = (url,callbackfn)=>{
+  const uploadDir = path.join(__dirname,'../',config.uploaddir);
+  const filename = `result_${uuid.v4()}.jpg`;
+  const filepath = `${uploadDir}/${filename}`;
+  const returi = `${config.uploadurl}/${filename}`;
+  console.log(`webshot-->打开${url}\n目标文件:${filepath}\n文件返回地址:${returi}`);
+
+  webshot(url,filepath,{
+          windowSize:
+          {
+            width: 640,
+            height: 1460
+          },
+          streamType:'jpg',
+          quality:90
+        },
+   (err)=> {
+      callbackfn(err,returi);
+    });
+};
+
+module.exports= snapimage;
