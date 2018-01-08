7ce4d7652348fa8db98f6a505320fba3165e7b30
wangxiaoqing <xiaoqingwang@126.com>
2018-01-08 19:01:38 +0800

    test only

--- deploy/test/puppteer.js
+++ deploy/test/puppteer.js
@@ -2,17 +2,25 @@
 // then creates a screenshot of the Diagram with makeImageData, plus a screenshot of the page.
 
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+// const fs = require('fs');
+//
+//
+// puppeteer.launch().then((browser)=>{
+//   browser.newPage().then((page)=>{
+//     page.goto('http://tmcat.czjcd.com/info/5a532bf5ca1c890001fb4d43', {
+//       waitUntil: 'networkidle2' // ensures images are loaded
+//     }).then(()=>{
+//       page.screenshot({ path: 'page-screenshot.png' }).then(()=>{
+//         browser.close();
+//       })
+//     })
+//   })
+// });
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+  await page.pdf({path: 'hn.pdf', format: 'A4'});
 
-
-puppeteer.launch().then((browser)=>{
-  browser.newPage().then((page)=>{
-    page.goto('http://tmcat.czjcd.com/info/5a532bf5ca1c890001fb4d43', {
-      waitUntil: 'networkidle2' // ensures images are loaded
-    }).then(()=>{
-      page.screenshot({ path: 'page-screenshot.png' }).then(()=>{
-        browser.close();
-      })
-    })
-  })
-});
+  await browser.close();
+})()
